544ec79200cbc3969cf96674c31a7028553cff03
cristhiantohasa <cristhian.tohasa@epn.edu.ec>
2022-03-01 21:07:07 -0500

    Se estructura el proyecto

--- maslow-web-frontend/src/app/app-routing.module.ts
+++ maslow-web-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ArchitectsComponent } from './components/architects/architects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'projects', component:ProjectsComponent},
+  {path:'architects', component:ArchitectsComponent},
+  {path:'aboutUs', component:AboutUsComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'**', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
