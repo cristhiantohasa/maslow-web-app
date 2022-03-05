db92d2c31e49cea89dacdcc6e782e647b6fdb0e8
cristhiantohasa <cristhian.tohasa@epn.edu.ec>
2022-03-04 19:15:13 -0500

    Se estructura el proyecto

--- maslow-web-frontend/src/app/app.module.ts
+++ maslow-web-frontend/src/app/app.module.ts
@@ -5,24 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/commons/header/header.component';
 import { FooterComponent } from './components/commons/footer/footer.component';
-import { SocialBarComponent } from './components/commons/social-bar/social-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ArchitectsComponent } from './components/architects/architects.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { SocialBarComponent } from './components/commons/social-bar/social-bar.component';
+import { FormComponent } from './components/commons/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SocialBarComponent,
     HomeComponent,
     ProjectsComponent,
     ArchitectsComponent,
     AboutUsComponent,
-    ContactComponent
+    ContactComponent,
+    SocialBarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
