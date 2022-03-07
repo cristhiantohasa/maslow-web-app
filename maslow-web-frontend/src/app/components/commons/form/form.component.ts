066d7ca05bb374a8e6bc0e834d6c0cbd2fdff78d
dalyzp15 <dalianazp15@gmail.com>
2022-03-07 05:01:31 -0500

    Form2

--- maslow-web-frontend/src/app/components/commons/form/form.component.ts
+++ maslow-web-frontend/src/app/components/commons/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  @ViewChild("nombre") nombre: any;
+  @ViewChild("correo") correo: any;
+  @ViewChild("proyectos") proyectos: any;
+  @ViewChild("financiamiento") financiamiento: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(form:NgForm){
+    localStorage.setItem("name", this.nombre.nativeElement.value)
+    localStorage.setItem("correo", this.correo.nativeElement.value)
+    localStorage.setItem("proyectos", this.proyectos.nativeElement.value)
+    localStorage.setItem("financiamiento", this.financiamiento.nativeElement.value)
+
   }
 
 }
