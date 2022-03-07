4de1784ccdf83401ed6301395b38c4d33e794208
Brayan Fernandez <brayan.fernandez@epn.edu.ec>
2022-03-07 01:58:28 -0500

    update/llamada a project
    Se logra llamar a cada project por su nombre

--- maslow-web-frontend/src/app/components/project/project.component.ts
+++ maslow-web-frontend/src/app/components/project/project.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Global } from 'src/app/services/global';
+import { ProjectService } from '../../services/project.service';
+import { Project } from '../../models/project';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+  providers:[ProjectService]
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  public url:string;
+  public project:Project;
+  public confirm:boolean;
+
+  constructor(
+    private _projectService:ProjectService,
+    private _router:Router,
+    private _route:ActivatedRoute
+  ) {
+    this.url=Global.url;
+    this.project=new Project('','','','','','',222,333,444,'');
+    this.confirm=false;
+   }
 
   ngOnInit(): void {
+    this._route.params.subscribe(params=>{
+      let name=params['name'];
+      console.log(name);
+      this.readProject(name);
+    }
+  );
   }
+
+  readProject(name:String){
+    this._projectService.readProject(name).subscribe(
+      response=>{
+        this.project=response.project;
+      },
+      error=>{
+        console.log(<any>error);
+      }
+    );
+  }
+
+  setConfirm(confirm:boolean){
+    this.confirm=confirm;
+  }
+
 
 }
